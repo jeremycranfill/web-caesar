c130e548c1f09de76bd986312f1e10e15e52588a
Jeremy <jeremy.cranfill@gmail.com>
2017-01-26 15:53:39 -0600

    added a lot of stuff

--- main.py
+++ main.py
@@ -16,15 +16,34 @@
 #
 import webapp2
 import caesar
+import cgi
+
+def build_page(text_area_content):
+    rotation_label = "<label>Rotate by how much</label>"
+    rotation_input = "<input type='number' name='rotation'/>"
+    message_label ="<label>Type a message:</label>"
+
+    textarea = "<textarea name='message'>" +text_area_content+ "</textarea>"
+    submit = "<input type ='submit'/>"
+    form = "<form method='post'>" + rotation_label + rotation_input +"<br>"+ message_label+ textarea + "<br>" + submit + "</form>"
+    header="<h2>Web Caesar</h2>"
+
+    return header + form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = "Hello World"
+
+        content = build_page("")
+
+        self.response.write(content)
+    def post(self):
+        rotation = int(self.request.get("rotation"))
+        message = self.request.get("message")
         encrypted_message = caesar.encrypt(message,13)
-        textarea = "<textarea>" +encrypted_message + "</textarea>"
-        submit = "<input type ='submit'/>"
-        form = "<form>" + textarea +submit + "</form>"
-        self.response.write(form)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(escaped_message)
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
